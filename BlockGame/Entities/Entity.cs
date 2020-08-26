b05a92cf5807c13ddb3fba5673da650bce112334
Globfish <51151906+Globfish@users.noreply.github.com>
2020-08-26 14:58:56 +0200

    entity

---
+++ BlockGame/Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockGame.Entities
+{
+    public interface Entity
+    {
+        int drawOrder { get; }
+
+        void Update(GameTime gameTime);
+
+        void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+
+        }
+    }
+}
